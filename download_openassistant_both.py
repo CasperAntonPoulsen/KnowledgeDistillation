a8045911473fe15e312cdb268dfc7198ea3219c5
CasperAntonPoulsen <75428146+CasperAntonPoulsen@users.noreply.github.com>
2024-01-03 10:48:41 +0100

    added jobs and evaluation

---
+++ download_openassistant_both.py
@@ -0,0 +1,19 @@
+from datasets import load_dataset
+import datasets
+import pandas as pd
+
+dataset = load_dataset("OpenAssistant/oasst1")
+
+df = dataset["train"].to_pandas()
+
+df_assistant = df[df["role"] == "assistant"][["message_id","parent_id","text","lang"]]
+df_prompter = df[(df["role"] == "prompter") & (df["parent_id"].isna()) ][["message_id","parent_id","text","lang"]]
+
+df_combined= df_assistant.merge( df_prompter, left_on="parent_id", right_on="message_id", suffixes= ["_response", "_prompt"])
+df_combined = df_combined.drop_duplicates(["parent_id_response"])
+df_combined["input"] = ""
+df_combined = df_combined[['text_prompt', "input",'text_response','lang_prompt']]
+df_combined.columns = ['instruction', "input", 'output', 'lang']
+print(len(df_combined))
+with open("/dtu/p1/johlau/LMOps/minillm/data/dolly_both/raw.jsonl", "w+") as file:
+    df_combined[df_combined["lang"].isin(["en", "es"])].to_json(file, orient="records", lines=True)
---
+++ download_wiki_dump_en.py
@@ -0,0 +1,14 @@
+from datasets import load_dataset
+import re
+
+dataset = load_dataset("graelo/wikipedia", "20230901.en", split='train')
+
+
+num = 0
+with open("/dtu/p1/johlau/LMOps/minillm/data/roberta_en/data.txt", "w") as f:
+    for data in dataset:
+        f.write(re.sub(r"\n+", "<@x(x!>", data['text']) + "\n")
+        num += 1
+
+print("Number of lines:", num)
+
